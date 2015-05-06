08822dcbbd8ce879c4687d90c81dd01713fdcc3c
Sam Pilla <sjpn96@mst.edu>
2015-05-06 14:25:34 -0500

    Item Array for minimizing code

---
+++ corelist.rb
@@ -0,0 +1,10 @@
+require './warframes'
+require './primaries'
+require './secondaries'
+require './melees'
+
+ITEMS = Array.new()
+ITEMS.push(WARFRAMES)
+ITEMS.push(PRIMARIES)
+ITEMS.push(SECONDARIES)
+ITEMS.push(MELEES)
